f1800cbf5deb0042a90be7073c509d592341bdd0
DivorcedLance <divorcedlance@gmail.com>
2024-11-24 18:01:14 -0500

    update base path for Vite configuration to version 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/molecular-dynamics-simulation/",
+  base: "/molecular-dynamics-simulation-v2/",
   build: {
     outDir: 'dist'
   },
